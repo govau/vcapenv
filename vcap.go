8a31ae81c6bc0e27f5188b4b67c83ef05e8c2079
Peter DeGroff <peter.degroff@digital.gov.au>
2020-07-03 16:48:53 +1000

    syslog: send to remote syslog url if provided

--- vcap.go
+++ vcap.go
@@ -5,6 +5,20 @@ import (
 	"fmt"
 )
 
+type VCAPApplication struct {
+	ApplicationID      string   `json:"application_id"`
+	ApplicationName    string   `json:"application_name"`
+	ApplicationURIs    []string `json:"application_uris"`
+	ApplicationVersion string   `json:"application_version"`
+	OrganizationID     string   `json:"organization_id"`
+	OrganizationName   string   `json:"organization_name"`
+	ProcessID          string   `json:"process_id"`
+	ProcessType        string   `json:"process_type"`
+	SpaceID            string   `json:"space_id"`
+	SpaceName          string   `json:"space_name"`
+	Version            string   `json:"version"`
+}
+
 type VCAPServices struct {
 	UserProvided []struct {
 		Name        string                     `json:"name"`
@@ -31,7 +45,6 @@ func (services VCAPServices) Environment(namer Namer) Environment {
 
 	for _, service := range services.UserProvided {
 		for credential, msg := range service.Credentials {
-
 			name := namer.Name(service.Name, credential)
 			env[name] = append(env[name], msg)
 		}
